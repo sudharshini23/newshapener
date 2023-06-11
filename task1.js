e4600d7f5759bba5db31ba8c44842f1c79c17166
sudharshini23 <sudarsh25@gmail.com>
2023-06-11 10:30:59 +0530

    second

---
+++ task1.js
@@ -0,0 +1,12 @@
+function Name() {
+    let Name = prompt('Enter your Name');
+}
+function Email() {
+    let email = prompt('Enter a valid email ID');
+}
+function Phone() {
+    let phone = prompt("Enter your phone number");
+}
+function DateTime() {
+    let date = prompt("Enter date and time: ");
+}
---
+++ task2-dom.js
@@ -0,0 +1,3 @@
+let items_new = document.getElementsByClassName('items');
+console.log(items_new);
+
